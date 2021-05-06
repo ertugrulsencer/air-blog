a612649a370ca60ff290b517eb44571530ed5856
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-05-07 00:47:32 +0300

    Post and Blog view created and route entegrated

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,21 @@ const routes = [
     component: Home,
   },
   {
+    path: "/blog",
+    name: "Blog",
+    component: () => import("../views/Blog.vue"),
+  },
+  {
+    path: "/post/:key",
+    name: "Post",
+    component: () => import("../views/Post.vue"),
+  },
+  {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
+  { path: "*", redirect: "/blog" },
 ];
 
 const router = new VueRouter({
